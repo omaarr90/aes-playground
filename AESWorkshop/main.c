93994bad2712414e976a815dbb2fe4463ba9aecb
Omar Alshammari <omar.alshammary@hotmail.com>
2018-05-14 23:17:17 +0300

    Initial Commit

---
+++ AESWorkshop/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AESWorkshop
+//
+//  Created by Omar Alshammari on 28/08/1439 AH.
+//  Copyright © 1439 Omar Alshammari. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
