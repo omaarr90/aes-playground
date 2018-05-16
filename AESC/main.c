ce7295a8d086b49f6531cbf4e2e256cfc637d8c4
Nahar <n.alkhodair@csmarc.sa>
2018-05-16 12:55:12 +0300

    Initial Commit

---
+++ AESC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AESC
+//
+//  Created by Nahar Alkhodair on 16/05/2018.
+//  Copyright © 2018 Nahar Alkhodair. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
